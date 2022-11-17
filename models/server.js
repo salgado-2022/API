c3d63b2455032711fd368d8e7359be7972739f35
nini 🌹 <86623637+SantiagoMesa0911@users.noreply.github.com>
2022-11-17 11:40:00 -0500

    first commit

--- models/server.js
+++ models/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const cors = require('cors')
-const {dbconnection} = require('../database/config')
+const dbconnection = require('../database/config')
 
 class Server{
     constructor(){
